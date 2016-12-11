487b280fece52e6b9c51df308ea7c29d4651783c
阿樊提 <fanjiahe2000@163.com>
2016-12-11 10:00:36 +0800

    Create install.scala

---
+++ ScriptLang/install.scala
@@ -0,0 +1,5 @@
+sudo apt-get remove scala-library scala
+wget http://www.scala-lang.org/files/archive/scala-2.11.6.deb
+sudo dpkg -i scala-2.11.6.deb
+sudo apt-get update
+sudo apt-get install scala
